f0bfdde3ee19ddfd7f80c4693f64bd0c327c3e87
Mohamed3amr <mohamedamrr666@gmail.com>
2025-02-22 21:51:36 +0200

    Assignment 1 (Lec 1)

--- WebSecService/resources/views/welcome.blade.php
+++ WebSecService/resources/views/welcome.blade.php
@@ -1,25 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap Test</title>
-    <link href="../../public/css/bootstrap.min.css" rel="stylesheet">
-    <script src="../../public/js/bootstrap.bundle.min.js"></script>
-</head>
-<body>
-    @php($j = 5)
-    <div class="card m-4 col-sm-2">
-        <div class="card-header">{{$j}} Multiplication Table</div>
-        <div class="card-body">
-            <table>
-                @foreach (range(1, 10) as $i)
-                    <tr>
-                        <td>{{$i}} * {{$j}}</td><td>= {{ $i * $j }}</td>
-                @endforeach
-            </table>
-        </div>
+@extends('layouts.master')
+@section('title', 'Welcome')
+@section('content')
+    <div class="card m-4">
+        <div class="card-header">Welcome To Home Page</div>
     </div>
-</body>
-
-</html>
+    </div>
+@endsection
--- WebSecService/routes/web.php
+++ WebSecService/routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/multable', function () {
+ return view('multable'); //multable.blade.php
+});
+Route::get('/even', function () {
+ return view('even'); //even.blade.php
+});
+Route::get('/prime', function () {
+ return view('prime'); //prime.blade.php
+});
